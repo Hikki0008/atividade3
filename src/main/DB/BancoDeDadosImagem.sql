2cb03e887639d83e129a8d9a973ce4bb02b5dd81
Hikki0008 <hikki008hachi@gmail.com>
2025-06-05 09:52:52 -0300

    Commit1

---
+++ src/main/DB/BancoDeDadosImagem.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE imagens_banco
+    DEFAULT CHARACTER SET = 'utf8mb4';
+    Use Imagens_banco;
+
+    CREATE TABLE imagem(
+        id BIGINT(20) NOT NULL AUTO_INCREMENT,
+        nome VARCHAR(100) NOT NULL,
+        url TEXT DEFAULT NULL,
+        PRIMARY kEY (id)
+    );
